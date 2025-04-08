f097c863afef1fa58f812aab3d469fed7820fe0f
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-04-08 20:15:58 +0700

    initial commit: setting project, splash screen

---
+++ app/src/main/java/com/example/onlineshop/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.onlineshop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.onlineshop.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(v ->
+                startActivity(new Intent(SplashActivity.this, MainActivity.class)));
+
+    }
+}
