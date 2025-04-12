d9df5208243f0f537bb011633a9ead662bb2f01e
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-04-12 23:03:38 +0700

    commit 8: item detail backend

--- app/src/main/java/com/example/onlineshop/Adapter/PopularAdapter.java
+++ app/src/main/java/com/example/onlineshop/Adapter/PopularAdapter.java
@@ -1,6 +1,7 @@
 package com.example.onlineshop.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterInside;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.onlineshop.Activity.DetailActivity;
 import com.example.onlineshop.Domain.ItemsModel;
 import com.example.onlineshop.databinding.ViewholderPopularBinding;
 
@@ -49,6 +51,12 @@ public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.Viewhold
                 .load(items.get(position).getPicUrl().get(0))
                 .apply(options)
                 .into(holder.binding.pic);
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.putExtra("object", items.get(position));
+            context.startActivity(intent);
+        });
     }
 
     @Override
